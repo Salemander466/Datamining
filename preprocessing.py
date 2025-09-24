f1d4399b8a02295ee2ff17de6c00341d3d91a9c5
Jose Lizana <lizanagalleguillos38@gmail.com>
2025-09-24 17:35:33 +0200

    preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,24 @@
-#Hello
+from sklearn.feature_extraction.text import CountVectorizer
+
+def vectorize_text(X_train, X_test, use_bigrams=False):
+    """
+    Make list of matrices using unigrams
+    and unigrams + bigrams
+    """
+
+    ngram_range = (1, 2) if use_bigrams else (1, 1)
+
+    vectorizer = CountVectorizer(
+        lowercase=True,
+        stop_words='english',
+        ngram_range=ngram_range,
+        max_df=0.95,
+        min_df=2,
+    )
+
+
+    # Train vectorizer and apply test
+    X_train = vectorizer.fit_transform(X_train)
+    X_test = vectorizer.transform(X_test)
+
+    return X_train, X_test, vectorizer
